26618ca31f2350216328949e3bb57683c6b0dc59
manu <manu156@users.noreply.github.com>
2023-06-17 12:28:00 +0530

    added constant (#16)
    Co-authored-by: manu.ekkati <manu.ekkati@paytm.com>

---
+++ src/main/java/com/github/manu156/jpqltosql/Const/Constants.java
@@ -0,0 +1,8 @@
+package com.github.manu156.jpqltosql.Const;
+
+import java.util.*;
+
+public class Constants {
+    public static Set<String> columnAnnotationNames = new HashSet<>(List.of("javax.persistence.Column"));
+    public static String tableAnnotationName = "javax.persistence.Table";
+}
